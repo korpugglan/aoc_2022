a8baf02f84c80fe2c5f5f77a8883bdd482be5f3e
korpugglan <alex.koerselman@gs1.nl>
2022-12-04 23:11:33 +0100

    Add solution for Day 4, PART II

--- scripts/04.py
+++ scripts/04.py
@@ -14,8 +14,25 @@ with open(input_file) as input_txt:
         if range_pair[0] == range_pair[2] or range_pair[1] == range_pair[3]:
             full_overlap_count += 1
         # If the left range is larger than the right range or vice versa, there is full overlap
-        elif (range_pair[0] < range_pair[2] and range_pair[1] > range_pair[3]) or (range_pair[0] > range_pair[2] and range_pair[1] < range_pair[3]):
+        elif (range_pair[0] < range_pair[2] and range_pair[1] > range_pair[3]) or \
+                (range_pair[0] > range_pair[2] and range_pair[1] < range_pair[3]):
             full_overlap_count += 1
         else:
             continue
     print(f"NUMBER OF FULL OVERLAP RANGES: {full_overlap_count}")
+
+# PART II
+partial_overlap_count = 0
+with open(input_file) as input_txt:
+    for row in input_txt:
+        row = row.strip()
+        range_pair = [int(x) for x in row.replace("-", ",").split(",")]
+        # If numbers match between the ranges, there is overlap
+        if any(x in range_pair[2:] for x in range_pair[:2]):
+            partial_overlap_count += 1
+        # If either starting number falls within the other range, there is overlap
+        elif range_pair[2] < range_pair[0] < range_pair[3] or range_pair[0] < range_pair[2] < range_pair[1]:
+            partial_overlap_count += 1
+        else:
+            continue
+    print(f"NUMBER OF PARTIAL OVERLAP RANGES: {partial_overlap_count}")
