7cde7e32fa9cdd1bdce83ecfb63719ccc0f9bc96
korpugglan <alex.koerselman@gs1.nl>
2022-12-06 12:34:31 +0100

    Add day 6 part II

--- scripts/06.py
+++ scripts/06.py
@@ -3,14 +3,18 @@
 input_file = "input_files/input_06.txt"
 
 # PART I
+# marker_length = 4
+# PART II
+marker_length = 14
+
 with open(input_file) as input_txt:
     signal = input_txt.read()
     input_length = len(signal)
     while True:
         # Test if the first 4 characters contain a duplicate. Remove a character and try again if that is the case
-        if len(signal[:4]) == len(set(signal[:4])):
+        if len(signal[:marker_length]) == len(set(signal[:marker_length])):
             break
         else:
             signal = signal[1:]
-    marker_found_at = input_length - len(signal) + 4
+    marker_found_at = input_length - len(signal) + marker_length
     print(f"First marker found after position {marker_found_at}")
