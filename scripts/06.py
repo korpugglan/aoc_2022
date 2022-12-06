052c5711aec0e0b6c9a36f7d61dd4837f1f92e80
korpugglan <alex.koerselman@gs1.nl>
2022-12-06 12:31:36 +0100

    Add day 6, part I

--- scripts/06.py
+++ scripts/06.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
-# Import packages
-
 # Define global variables
 input_file = "input_files/input_06.txt"
 
 # PART I
 with open(input_file) as input_txt:
-    for row in input_txt:
-        row = row.strip()
+    signal = input_txt.read()
+    input_length = len(signal)
+    while True:
+        # Test if the first 4 characters contain a duplicate. Remove a character and try again if that is the case
+        if len(signal[:4]) == len(set(signal[:4])):
+            break
+        else:
+            signal = signal[1:]
+    marker_found_at = input_length - len(signal) + 4
+    print(f"First marker found after position {marker_found_at}")
